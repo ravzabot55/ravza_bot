3b754f1b85af13fbd946b6b2fed11aa338ed4292
ravzabot55 <barroibnozib@gmail.com>
2025-03-18 22:19:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+# main.py (asosiy fayl)
+from aiogram import Bot, Dispatcher, executor, types
+from data.config import BOT_TOKEN
+from handlers import start, menu, ravza, food, hotel, faq, bozor
 
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+# Har bir bo'limni ro'yxatdan o'tkazamiz
+start.register_handlers_start(dp)
+menu.register_handlers_menu(dp)
+ravza.register_handlers_ravza(dp)
+hotel.register_handlers_hotel(dp)
+food.register_handlers_food(dp)
+faq.register_handlers_faq(dp)
+bozor.register_handlers_bozor(dp)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
